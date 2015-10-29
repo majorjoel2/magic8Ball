7766e26a7efac954d69625230cc27b1766231554
majorjoel2 <joel@tanner-family.org>
2015-10-29 09:14:39 -0400

    Negetive 8 Ball

--- src/magic8Ball/Magic8.java
+++ src/magic8Ball/Magic8.java
@@ -13,17 +13,22 @@ public class Magic8 {
 		// Fill the array with at least five potential answers
 		String[] answers = new String[6];
 		answers[0] = "Yes";
-		answers[1] = "No";
+		answers[1] = "Most Likely";
 		answers[2] = "Reply Hazy, try again later";
 		answers[3] = "Cannot Predict Now";
-		answers[4] = "Most Likely";
+		answers[4] = "No";
 		answers[5] = "Probably Not";
 		
 		// Choose one of the answers at random, then print it
 		int min = 0;
+		int min2 = 4;
 		int max = 5;
 		Random Random = new Random();
 		int RandomNumber = Random.nextInt(max - min + 1) + min;
+		int RandomNumber2 = Random.nextInt(max - min + 1) + min;
+		if(RandomNumber == RandomNumber2) {
+			RandomNumber = Random.nextInt(max - min2 + 1) + min2;
+		}
 		String Output = answers[RandomNumber].toString();
 		System.out.println(Output);
 	}
